caaa82c94ec44c0428e5a59409106831c6008b23
shubham6822 <sharmashubham6822@gmail.com>
2024-08-13 16:22:09 +0530

    header created

--- src/app/(root)/document/[id]/page.tsx
+++ src/app/(root)/document/[id]/page.tsx
@@ -1,9 +1,15 @@
 import { Editor } from '@/components/editor/Editor'
+import Header from '@/components/Header'
 import React from 'react'
 
 const Document = () => {
     return (
         <div>
+            <Header>
+                <div className='flex w-fit items-center justify-center gap-2'>
+                    <h1 className='text-xl'>this is fake </h1>
+                </div>
+            </Header>
             <Editor />
         </div>
     )
